0aea6849765025ba5997a160103e6060bd32bc36
buncis mamen <buncismamen@gmail.com>
2016-10-02 00:48:31 +0700

    add validate

--- app/models/mataharinya.rb
+++ app/models/mataharinya.rb
@@ -1,23 +1,33 @@
 class Mataharinya < ApplicationRecord
-  # def hasilnya
-  #   when variabel_bebas.true?
-  #     "regresi"
-  #   when dua_variabel_keputusan.true? || linier_programming.true?
-  #     "grafik "
-  #   when garis_lurus.true? || garis_tren.true?
-  #     "proyeksi_tren"
-  #   when populasi_objek.true?
-  #     "survei_lengkap"
-  #   when banyak_staff.true? || kemampuan_analisis.true?
-  #     "survei_delphi "
-  #   when wawancara_langsung.true? || opini_masyarakat.true?
-  #     "survei_opini "
-  #   when waktu_singkat.true? || sebagian_objek.true?
-  #     "survei_sample"
-  #   when memperagakan_kegiatan.true? || imbalan_koresponden.true?
-  #     "simulasi_pasar"
-  #   when praktek.true?
-  #     "uji_pasar"
-  #   end
-  # end
+  validates :judul, presence: true
+  validate :validasi_tahap_1
+  validate :validasi_tahap_2
+  validate :validasi_tahap_3
+  validate :validasi_tahap_4
+  validate :validasi_tahap_5
+  validate :validasi_tahap_6
+
+  def validasi_tahap_1
+    errors.add(:description, "pilihan lo salah bruh") if (model_persamaan || data_historis || variable && intuisi || subjektif)
+  end
+
+  def validasi_tahap_2
+    errors.add(:description, "pilihan lo salah bruh") if (waktu || periodisasi && variable_bebas)
+  end
+
+  def validasi_tahap_3
+    errors.add(:description, "pilihan lo salah bruh") if (kuesioner || alat_interview && uji_coba)
+  end
+
+  def validasi_tahap_4
+    errors.add(:description, "pilihan lo salah bruh") if (dua_variabel_keputusan || linier_programming && garis_tren || garis_lurus )
+  end
+
+  def validasi_tahap_5
+    errors.add(:description, "pilihan lo salah bruh") if (populasi_objek && banyak_staff || kemampuan_analisis && wawancara_langsung || opini_masyarakat && waktu_singkat || sebagian_objek )
+  end
+
+  def validasi_tahap_6
+    errors.add(:description, "pilihan lo salah bruh") if (imbalan_koresponden || memperagakan_kegiatan && praktek)
+  end
 end
